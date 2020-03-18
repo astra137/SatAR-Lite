8a59bfa768501948c06945b6e9b065eff1c00e14
Mac <enkindled@pm.me>
2020-03-17 17:58:48 -0700

    Add search to satellite list
    Add a navigation controller and use it to show a search bar above the tableview.

--- SatAR Lite/SatelliteListTableViewController.swift
+++ SatAR Lite/SatelliteListTableViewController.swift
@@ -1,18 +1,52 @@
 import Foundation
 import UIKit
 
-class SatelliteListViewController: UITableViewController {
-
+class SatelliteListViewController: UITableViewController, UISearchResultsUpdating {
+    
+    var filtered: [AmateurRadioSatellite] = []
+    
+    override func viewDidLoad() {
+        let searchController = UISearchController(searchResultsController: nil)
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search name or ID"
+        searchController.searchBar.sizeToFit()
+        
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+        filtered = Cache.list
+    }
+    
+    func filterContentForSearchText(_ searchText: String) {
+        // TODO: NSPredicate?
+        filtered = searchText.isEmpty ? Cache.list : Cache.list.filter { ars in
+            return ars.tle.commonName.lowercased().contains(searchText.lowercased())
+                || String(ars.tle.noradIndex).contains(searchText.lowercased())
+        }
+        
+        tableView.reloadData()
+    }
+    
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        filterContentForSearchText(searchBar.text!)
+    }
+    
+    // MARK: - UITableViewDataSource
+    
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // List all satellites, even untracked ones
-        return Cache.list.count
+        return filtered.count
     }
+    
+    // MARK: - UITableViewDelegate
     
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = UITableViewCell(style: UITableViewCell.CellStyle.default, reuseIdentifier: "cell")
         
         // Populate the name and visibility checkmark
-        let sat = Cache.list[indexPath.row]
+        let sat = filtered[indexPath.row]
         
         cell.textLabel?.text = sat.tle.commonName
         
@@ -26,8 +60,10 @@ class SatelliteListViewController: UITableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         // UITableViewCell.AccessoryType.<acc> may be useful
         
-        // Toggle checkmark by mutating the struct in the list directly
-        let sat = Cache.list[indexPath.row]
+        let sat = filtered[indexPath.row]
+        
+        // Toggle checkmark by mutating the class in the list directly
+        // This is a reference to the object in the Cache.list, so that will change too
         sat.tracking = !sat.tracking
         
         if sat.tracking {
